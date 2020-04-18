93feae46cb524f0c9ee7731d7a464810ed3412d1
elevliaykut <elevli.aykut.28@gmail.com>
2020-04-18 13:16:58 +0300

    destructuring

---
+++ challenges/destructuring-js/destructuring.js
@@ -0,0 +1,22 @@
+const person = {
+    name: 'Max',
+    age: 29,
+    greet() {
+      console.log('Hi, I am ' + this.name);
+    }
+  };
+  
+  const printName = ({ name }) => {
+    console.log(name);
+  };
+  
+  printName(person);
+  
+  const { name, age } = person;
+  console.log(name, age);
+  
+  const hobbies = ['Sports', 'Cooking'];
+  const [hobby1, hobby2] = hobbies;
+  console.log(hobby1, hobby2);
+
+  
