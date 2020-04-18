c07030e4535d94c5842617525c61dde913a92325
elevliaykut <elevli.aykut.28@gmail.com>
2020-04-18 13:01:51 +0300

    core-js-syntax added

---
+++ challenges/core-js-syntax/core-js-syntax.js
@@ -0,0 +1,17 @@
+var name = 'Max';
+var age = 29;
+
+var hashobbies = true;
+
+function summarizeUser(userName,userAge,userHasHobby) {
+    return (
+        'Name is' +
+        username +
+        ' age is a' +
+        userAge +
+        ' and user has hobbies ' +
+        userHasHobby
+    );
+}
+
+console.log(summarizeUser(name,age,hashobbies));
