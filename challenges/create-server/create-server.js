7b0bd4cff4380702de44f6ac3cb045f76efbb494
elevliaykut <elevli.aykut.28@gmail.com>
2020-04-18 14:45:39 +0300

    create server improvements

--- challenges/create-server/create-server.js
+++ challenges/create-server/create-server.js
@@ -1,7 +1,13 @@
 const http = require('http');
 
 const server = http.createServer((req,res) => {
-    console.log("Listening Request" + req);
+    console.log("Listening Request" + req.url, req.method + req.headers);
+    res.setHeader('Content-Type','text/html');
+    res.write('<html>');
+    res.write('<head><title>Server Page</title></head>');
+    res.write('<body><h1>Hello from my node.js Server!</h1></body>');
+    res.write('</html>');
+    res.end();
 });
 
 server.listen(3000);
